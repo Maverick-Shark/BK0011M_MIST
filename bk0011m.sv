2e3e2278b2b625329bdf4ed872f325bb7586666e
Maverick-Shark <rataweb@gmail.com>
2024-04-28 06:11:22 +0200

    DeMiSTify Poseidon board

--- bk0011m.sv
+++ bk0011m.sv
@@ -21,27 +21,59 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-module bk0011m
+// module bk0011m
+module guest_top
 (
    input         CLOCK_27, // Input clock 27 MHz
+`ifdef USE_CLOCK_50
+	input         CLOCK_50,
+`endif
 
-   output  [5:0] VGA_R,
-   output  [5:0] VGA_G,
-   output  [5:0] VGA_B,
+   output [VGA_BITS-1:0] VGA_R,
+   output [VGA_BITS-1:0] VGA_G,
+   output [VGA_BITS-1:0] VGA_B,
    output        VGA_HS,
    output        VGA_VS,
 	 
    output        LED,
 
+`ifdef USE_HDMI
+   output        HDMI_RST,
+   output  [7:0] HDMI_R,
+   output  [7:0] HDMI_G,
+   output  [7:0] HDMI_B,
+   output        HDMI_HS,
+   output        HDMI_VS,
+   output        HDMI_PCLK,
+   output        HDMI_DE,
+   inout         HDMI_SDA,
+   inout         HDMI_SCL,
+   input         HDMI_INT,
+`endif
+
    output        AUDIO_L,
    output        AUDIO_R,
+`ifdef I2S_AUDIO
+   output        I2S_BCK,
+   output        I2S_LRCK,
+   output        I2S_DATA,
+`endif
 
    input         SPI_SCK,
    output        SPI_DO,
    input         SPI_DI,
-   input         SPI_SS2,
-   input         SPI_SS3,
-   input         CONF_DATA0,
+   input         SPI_SS2,    // data_io
+   input         SPI_SS3,    // OSD
+   input         CONF_DATA0, // SPI_SS for user_io
+
+`ifdef USE_QSPI
+   input         QSCK,
+   input         QCSn,
+   inout   [3:0] QDAT,
+`endif
+`ifndef NO_DIRECT_UPLOAD
+   input         SPI_SS4,
+`endif
 
    output [12:0] SDRAM_A,
    inout  [15:0] SDRAM_DQ,
@@ -53,9 +85,72 @@ module bk0011m
    output        SDRAM_nCS,
    output  [1:0] SDRAM_BA,
    output        SDRAM_CLK,
-   output        SDRAM_CKE
+   output        SDRAM_CKE,
+
+`ifdef DUAL_SDRAM
+   output [12:0] SDRAM2_A,
+   inout  [15:0] SDRAM2_DQ,
+   output        SDRAM2_DQML,
+   output        SDRAM2_DQMH,
+   output        SDRAM2_nWE,
+   output        SDRAM2_nCAS,
+   output        SDRAM2_nRAS,
+   output        SDRAM2_nCS,
+   output  [1:0] SDRAM2_BA,
+   output        SDRAM2_CLK,
+   output        SDRAM2_CKE,
+`endif
+
+`ifdef I2S_AUDIO_HDMI
+   output        HDMI_MCLK,
+   output        HDMI_BCK,
+   output        HDMI_LRCK,
+   output        HDMI_SDATA,
+`endif
+`ifdef SPDIF_AUDIO
+   output        SPDIF,
+`endif
+`ifdef USE_AUDIO_IN
+   input         AUDIO_IN,
+`endif
+   input         UART_RX,
+   output        UART_TX
 );
 
+`ifdef NO_DIRECT_UPLOAD
+localparam bit DIRECT_UPLOAD = 0;
+wire SPI_SS4 = 1;
+`else
+localparam bit DIRECT_UPLOAD = 1;
+`endif
+
+`ifdef USE_QSPI
+localparam bit QSPI = 1;
+assign QDAT = 4'hZ;
+`else
+localparam bit QSPI = 0;
+`endif
+
+`ifdef VGA_8BIT
+localparam VGA_BITS = 8;
+`else
+localparam VGA_BITS = 6;
+`endif
+
+`ifdef USE_HDMI
+localparam bit HDMI = 1;
+assign HDMI_RST = 1'b1;
+`else
+localparam bit HDMI = 0;
+`endif
+
+`ifdef BIG_OSD
+localparam bit BIG_OSD = 1;
+`define SEP "-;",
+`else
+localparam bit BIG_OSD = 0;
+`define SEP
+`endif
 
 /////////////////////////////   CLOCKS   //////////////////////////////
 wire plock;
@@ -134,10 +229,11 @@ wire        sd_buff_wr;
 wire        sd_mounted;
 
 `include "build_id.v"
+
 localparam CONF_STR = 
 {
 	"BK0011M;BINDSK;",
-	"S3,VHD;",
+	"S3U,VHD,Mount HDD;",
 	"O78,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%;",
 	"O1,CPU Speed,3MHz/4MHz,6MHz/8MHz;",
 	"O56,Model,BK0011M & DSK,BK0010 & DSK,BK0011M,BK0010;",
--- clean.bat
+++ clean.bat
@@ -2,15 +2,26 @@
 del /s *.bak
 del /s *.orig
 del /s *.rej
+
 rmdir /s /q db
 rmdir /s /q incremental_db
 rmdir /s /q output_files
 rmdir /s /q simulation
 rmdir /s /q greybox_tmp
+
+rmdir /s /q poseidon-ep4cgx150\db
+rmdir /s /q poseidon-ep4cgx150\incremental_db
+rmdir /s /q poseidon-ep4cgx150\output_files
+rmdir /s /q poseidon-ep4cgx150\simulation
+rmdir /s /q poseidon-ep4cgx150\greybox_tmp
+
 del PLLJ_PLLSPE_INFO.txt
 del *.qws
 del *.ppf
-del *.qip
 del *.ddb
 del build_id.v
+
+del poseidon-ep4cgx150\*.qws
+del poseidon-ep4cgx150\build_id.v
+
 pause
